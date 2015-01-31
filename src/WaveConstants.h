aa02b47b1326c844f9b9f4030f86756906483511
2826WaveRobotics <kelliewatters@gmail.com>
2015-01-30 21:36:29 -0600

    merging changes
    trying to merge test and reset branches

--- src/WaveConstants.h
+++ src/WaveConstants.h
@@ -8,11 +8,21 @@ const double lowElevatorPosition=1.0;
 const int On=1;
 const int Off=0;
 
+
+//An enumeration is a numbered list of variables
 enum
 {
 	Bin_CenterConfig = 0,
-	Bin_LeftConfig,
-	Bin_RightConfig
+	Bin_LeftConfig, //This has a value of 1
+	Bin_RightConfig //This has a value of 2
+};
+
+
+enum
+{
+	Bin_LiftCylinder = 0,
+	Bin_LeftLock, //This has a value of 1
+	Bin_RightLock //This has a value of 2
 };
 
 #endif
